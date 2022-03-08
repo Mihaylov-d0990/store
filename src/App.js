847ac1b1b98ad5378719a01c3ee5988e613bfcc9
Danila <syvs0990@mail.ru>
2022-03-08 22:08:59 +0700

    Delete extra files and add main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,261 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div class="wrapper">
+            <div class="head dividing">
+                <div class="container">
+                    <div class="head__content">
+                        <div class="head__top">
+                            <div class="head__logo">
+                                <a href="/store-layout.github.io/">
+                                    Store-layout
+                                </a>
+                            </div>
+                            <div class="head__search">
+                                <input type="text" />
+                                <button>Search</button>
+                            </div>
+                            <div class="head__profile">
+                                <a href="login.html">
+                                    <div class="head__login">
+                                        <img src="images/profile.svg" alt="" />
+                                    </div>
+                                </a>
+                                <a href="cart.html">
+                                    <div class="head__basket">
+                                        <img src="images/cart.svg" alt="" />
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="head__nav">
+                            <nav>
+                                <a href="catalog.html">
+                                    <div class="head__link">
+                                        Catalog
+                                    </div>
+                                </a>
+                                <a href="/store-layout.github.io/">
+                                    <div class="head__link">
+                                        Lorem.
+                                    </div>
+                                </a>
+                                <a href="/store-layout.github.io/">
+                                    <div class="head__link">
+                                        Lorem.
+                                    </div>
+                                </a>
+                                <a href="/store-layout.github.io/">
+                                    <div class="head__link">
+                                        Lorem.
+                                    </div>
+                                </a>
+                                <a href="/store-layout.github.io/">
+                                    <div class="head__link">
+                                        Lorem.
+                                    </div>
+                                </a>
+                                <a href="/store-layout.github.io/">
+                                    <div class="head__link">
+                                        Lorem.
+                                    </div>
+                                </a>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="main">
+                <div class="container">
+                    <div class="main__content">
+                        <div class="main__title title">Lorem, ipsum dolor.</div>
+                        <div class="main__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo adipisci autem culpa consequuntur doloremque voluptas aut in, libero sapiente nihil suscipit quaerat? Accusantium nostrum, nobis tempora debitis nulla officiis corrupti recusandae tenetur non mollitia illum quas maxime obcaecati dicta cupiditate culpa quaerat dolorem delectus dignissimos quis ipsum eveniet aliquid consequatur. Nulla ad quae harum quisquam. Cumque nulla dolorem velit id maxime aspernatur deleniti aliquam magni adipisci enim, vel perspiciatis. Ad, repellendus fuga inventore qui veniam voluptatum provident aliquam libero dolore fugiat animi quis earum sunt deleniti eius tenetur excepturi eveniet pariatur culpa optio odio facere. Velit atque tempore facilis. At!</div>
+                    </div>
+                </div>
+            </div>
+            <div class="dividing"></div>
+            <div class="favourites">
+                <div class="container">
+                    <div class="favourites__content">
+                        <div class="favourites__item">
+                            <a href="product.html" class="favourites__image">
+                                <img src="images/image.jpg" alt="" />
+                            </a>
+                            <div class="favourites__item-bottom">
+                                <div class="favourites__info">
+                                    <div class="favourites__text">Obi belt</div>
+                                    <div class="favourites__price">19$ US</div>
+                                </div>
+                                <div class="favourites__cart">
+                                    <img src="images/cart.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="favourites__item">
+                            <a href="product.html" class="favourites__image">
+                                <img src="images/image.jpg" alt="" />
+                            </a>
+                            <div class="favourites__item-bottom">
+                                <div class="favourites__info">
+                                    <div class="favourites__text">Obi belt</div>
+                                    <div class="favourites__price">19$ US</div>
+                                </div>
+                                <div class="favourites__cart">
+                                    <img src="images/cart.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="favourites__item">
+                            <a href="product.html" class="favourites__image">
+                                <img src="images/image.jpg" alt="" />
+                            </a>
+                            <div class="favourites__item-bottom">
+                                <div class="favourites__info">
+                                    <div class="favourites__text">Obi belt</div>
+                                    <div class="favourites__price">19$ US</div>
+                                </div>
+                                <div class="favourites__cart">
+                                    <img src="images/cart.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="favourites__item">
+                            <a href="product.html" class="favourites__image">
+                                <img src="images/image.jpg" alt="" />
+                            </a>
+                            <div class="favourites__item-bottom">
+                                <div class="favourites__info">
+                                    <div class="favourites__text">Obi belt</div>
+                                    <div class="favourites__price">19$ US</div>
+                                </div>
+                                <div class="favourites__cart">
+                                    <img src="images/cart.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="favourites__item">
+                            <a href="product.html" class="favourites__image">
+                                <img src="images/image.jpg" alt="" />
+                            </a>
+                            <div class="favourites__item-bottom">
+                                <div class="favourites__info">
+                                    <div class="favourites__text">Obi belt</div>
+                                    <div class="favourites__price">19$ US</div>
+                                </div>
+                                <div class="favourites__cart">
+                                    <img src="images/cart.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="favourites__item">
+                            <a href="product.html" class="favourites__image">
+                                <img src="images/image.jpg" alt="" />
+                            </a>
+                            <div class="favourites__item-bottom">
+                                <div class="favourites__info">
+                                    <div class="favourites__text">Obi belt</div>
+                                    <div class="favourites__price">19$ US</div>
+                                </div>
+                                <div class="favourites__cart">
+                                    <img src="images/cart.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="favourites__item">
+                            <a href="product.html" class="favourites__image">
+                                <img src="images/image.jpg" alt="" />
+                            </a>
+                            <div class="favourites__item-bottom">
+                                <div class="favourites__info">
+                                    <div class="favourites__text">Obi belt</div>
+                                    <div class="favourites__price">19$ US</div>
+                                </div>
+                                <div class="favourites__cart">
+                                    <img src="images/cart.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="favourites__item">
+                            <a href="product.html" class="favourites__image">
+                                <img src="images/image.jpg" alt="" />
+                            </a>
+                            <div class="favourites__item-bottom">
+                                <div class="favourites__info">
+                                    <div class="favourites__text">Obi belt</div>
+                                    <div class="favourites__price">19$ US</div>
+                                </div>
+                                <div class="favourites__cart">
+                                    <img src="images/cart.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="favourites__item">
+                            <a href="product.html" class="favourites__image">
+                                <img src="images/image.jpg" alt="" />
+                            </a>
+                            <div class="favourites__item-bottom">
+                                <div class="favourites__info">
+                                    <div class="favourites__text">Obi belt</div>
+                                    <div class="favourites__price">19$ US</div>
+                                </div>
+                                <div class="favourites__cart">
+                                    <img src="images/cart.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="favourites__item">
+                            <a href="product.html" class="favourites__image">
+                                <img src="images/image.jpg" alt="" />
+                            </a>
+                            <div class="favourites__item-bottom">
+                                <div class="favourites__info">
+                                    <div class="favourites__text">Obi belt</div>
+                                    <div class="favourites__price">19$ US</div>
+                                </div>
+                                <div class="favourites__cart">
+                                    <img src="images/cart.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="foot dividing">
+                <div class="container">
+                    <div class="foot__content">
+                        <div class="foot__columns">
+                            <div class="foot__column">
+                                <div class="foot__item">Lorem, ipsum.</div>
+                                <div class="foot__item">Lorem, ipsum.</div>
+                                <div class="foot__item">Lorem, ipsum.</div>
+                                <div class="foot__item">Lorem, ipsum.</div>
+                            </div>
+                            <div class="foot__column">
+                                <div class="foot__item">Lorem, ipsum.</div>
+                                <div class="foot__item">Lorem, ipsum.</div>
+                                <div class="foot__item">Lorem, ipsum.</div>
+                                <div class="foot__item">Lorem, ipsum.</div>
+                            </div>
+                            <div class="foot__column">
+                                <div class="foot__item">Lorem, ipsum.</div>
+                                <div class="foot__item">Lorem, ipsum.</div>
+                                <div class="foot__item">Lorem, ipsum.</div>
+                                <div class="foot__item">Lorem, ipsum.</div>
+                            </div>
+                            <div class="foot__column">
+                                <div class="foot__item">Lorem, ipsum.</div>
+                                <div class="foot__item">Lorem, ipsum.</div>
+                                <div class="foot__item">Lorem, ipsum.</div>
+                                <div class="foot__item">Lorem, ipsum.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
