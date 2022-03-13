9007ecf176d913ebcfc9b1e09e974380597bef9e
Danila <syvs0990@mail.ru>
2022-03-13 23:37:02 +0700

    Fix images and add the local storage

--- components/Header.jsx
+++ components/Header.jsx
@@ -59,6 +59,7 @@ export default function Header() {
         setToggleNav(() => !toggleNav)
     }
 
+
     return (
         <>
             <Head>
--- components/List.jsx
+++ components/List.jsx
@@ -5,8 +5,6 @@ import Image from "next/image"
 
 export default function List() {
 
-    
-
     const list = React.useState(() => {
         let arr = []
         for (let i = 0; i < 10; i++) {
@@ -21,6 +19,18 @@ export default function List() {
 
         return arr
     })[0]
+
+    const addToCart = (id) => {
+        if (typeof localStorage.cart === "undefined") {
+            localStorage.cart = JSON.stringify({id: id})
+        } else {
+            const locS = JSON.parse(localStorage.cart)
+            let obj = {id: 1}
+            locS = {locS, obj}
+            localStorage.cart = JSON.stringify(locS)
+        }
+        console.log(JSON.parse(localStorage.cart))
+    }
 
     return (
         <div className="list">
@@ -39,7 +49,7 @@ export default function List() {
                                 <div className="list__price">Price: <span>{item.price}$ US</span></div>
                             </div>
                             <div className="list__cart">
-                                <div>
+                                <div className="list__cart-image" onClick={addToCart}>
                                     <Image src="/images/cart.svg"  width="24px" height="24px" alt="cart icon" />
                                 </div>
                             </div>
